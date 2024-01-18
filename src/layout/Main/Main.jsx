3aca1223ef1cc5a653b3d6b19139039f65eaf652
alihasanlitonislam <alihasan404439@gmail.com>
2024-01-18 14:48:32 +0600

    create the stage brench and uplode the github "

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
     return (
         <div>
             <Outlet/>
-            hello word
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ad, consequatur voluptatibus reiciendis pariatur blanditiis quis aliquid cum. Placeat deserunt sed nulla fugiat similique fugit. Harum molestias nemo ullam veniam.</h1>
         </div>
     );
 };
