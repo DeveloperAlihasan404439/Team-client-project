a5ab540c2cb13544a7e3e35d278e962775e092d0
Moshiur Rahman <moshiurrahmansourave@gmail.com>
2024-01-18 21:47:15 +0600

    testing

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
     return (
         <div>
             <Outlet/>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ad, consequatur voluptatibus reiciendis pariatur blanditiis quis aliquid cum. Placeat deserunt sed nulla fugiat similique fugit. Harum molestias nemo ullam veniam.</h1>
+            <h1 className="text-2xl text-red-400">fokira developer amra vi</h1>
         </div>
     );
 };
