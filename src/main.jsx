463c2c338513532ac7be52919f2ec2a22a9713b8
Moshiur Rahman <moshiurrahmansourave@gmail.com>
2024-01-19 16:46:29 +0600

    add the authentication system

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/Routers.jsx'
+import AuthProvider from './provider/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router}/>
+    </AuthProvider>
   </React.StrictMode>,
 )
